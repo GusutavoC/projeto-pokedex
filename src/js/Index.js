8ce301df775c2340257ea4b1ce93d31c6e007092
GustavoC <133548728+GusutavoC@users.noreply.github.com>
2023-05-14 17:36:08 -0300

    Add files via upload

--- src/js/Index.js
+++ src/js/Index.js
@@ -14,25 +14,20 @@
 */
 
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
-
 const body = document.querySelector("body");
-console.log(body);
-
-const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+const imagemBotaoTrocaDeTema = document.
+    querySelector(".imagem-botao");
 
 botaoAlterarTema.addEventListener("click", () => {
 
-    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+    const modoEscuroEstaAtivo = body.classList.
+        contains("modo-escuro");
 
-body.classList.toggle("modo-escuro")
+    body.classList.toggle("modo-escuro")
 
     if (modoEscuroEstaAtivo) {
-
-
         imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png")
-
     } else {
-
         imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
     };
 
